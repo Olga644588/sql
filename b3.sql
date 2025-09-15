2d216b90e01b3c483b4e3b9414fb70a151ff381c
Olga644588 <os3634067@gmail.com>
2025-09-15 23:27:25 +0500

    cheange

--- b3.sql
+++ b3.sql
@@ -1,46 +1,52 @@
-CREATE TABLE genres(
-genre_id SERIAL PRIMARY KEY,
-name VARCHAR(100) NOT NULL
-);
-CREATE TABLE artists(
-artist_id SERIAL PRIMARY KEY,
-name VARCHAR(100) NOT NULL
-);
-CREATE TABLE artists_genres(
-artist_id INTEGER NOT NULL,
-genre_id INTEGER NOT NULL,
-PRIMARY KEY(artist_id, genre_id),
-FOREIGN KEY(artist_id) REFERENCES artists(artist_id),
-FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
-);
-CREATE TABLE albums(
-album_id SERIAL PRIMARY KEY,
-title VARCHAR(100) NOT NULL,
-release_date DATE
-);
-CREATE TABLE artists_albums(
-artist_id INTEGER NOT NULL,
-album_id INTEGER NOT NULL,
-PRIMARY KEY(artist_id, album_id),
-FOREIGN KEY(artist_id) REFERENCES artists(artist_id),
-FOREIGN KEY(album_id) REFERENCES albums(album_id)
-);
-CREATE TABLE tracks(
-track_id SERIAL PRIMARY KEY,
-title VARCHAR(100) NOT NULL,
-duration TIME,
-album_id INTEGER NOT NULL,
-FOREIGN KEY(album_id) REFERENCES albums(album_id)
-);
-CREATE TABLE compilation(
-compilation_id SERIAL PRIMARY KEY,
-title VARCHAR(120) NOT NULL,
-release_year INTEGER NOT NULL
-);
-CREATE TABLE compilations_tracks(
-compilation_id INTEGER NOT NULL,
-track_id INTEGER NOT NULL,
-PRIMARY KEY(compilation_id, track_id),
-FOREIGN KEY(compilation_id) REFERENCES compilations(compilation_id),
-FOREIGN KEY(track_id) REFERENCES tracks(track_id)
-);
+ДЗ 1
+INSERT INTO genres(name) VALUES
+("Рок"),
+("Поп"),
+("Народный");
+INSERT INTO artists(name) VALUES
+("Иван Иванов"),
+("Ирина Кузнецова"),
+("Диана"),
+("Петр Смирнов");
+INSERT INTO artists_genres(artist_id, genre_id) VALUES
+(1,1),
+(1,2),
+(2,2),
+(3,3),
+(4,1),
+(4,3);
+INSERT INTO albums(title, year) VALUES
+("Звездопад", 2020),
+("Новинки", 2021),
+("Вечера", 2022);
+INSERT INTO albums_artists(album_id, artist_id) VALUES
+(1,1),
+(1,2),
+(2,2),
+(2,4),
+(3,3),
+(3,4);
+INSERT INTO tracks(title, duration_seconds, album_id) VALUES
+("Морозное утро", 210, 1),
+("Ветерок", 200, 1),
+("Солнышко", 220, 2),
+("Мелодия", 180,2),
+("Закат", 190, 3),
+("Мой любимый город", 215, 3),
+("Новинка", 211, 2);
+INSERT INTO compilations(title, year) VALUES
+("Лучшее", 2020),
+("Хиты", 2021),
+("Избранное", 2022),
+("Золотые хиты", 2023);
+INSERT INTO compilations_tracks(compilation_id, track_id) VALUES
+(1,1),
+(1,3),
+(2,1),
+(2,2),
+(2,4),
+(3,5),
+(3,6),
+(4,2),
+(4,4),
+(4,5);
